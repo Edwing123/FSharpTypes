dd1e5a2caffe0bd0d2ff290e9dd860ac2b1ef290
Edwin Garcia <egarciavalle2014@gmail.com>
2025-02-13 21:09:11 -0600

    chore: finish tuples

---
+++ Tuples.fs
@@ -0,0 +1,9 @@
+module FSharpTypes.Tuples
+
+// Tuples: the cartisian product of two or more types.
+
+// A list of two ints... or the combination
+// of all possible integers.
+type Coords = int * int
+
+// Ahhh...
